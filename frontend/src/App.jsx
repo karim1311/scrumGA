02e8d2fea52d4bd48cb20e933e71c0915b73f52b
Josue Pena <josuemorianp@gmail.com>
2023-10-27 12:17:24 -0400

    prueba de conexion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+const [count, setCount] = useState(0)
+
+async function conectarBackend() {
+  const response = await fetch("http://localhost:8000/api/alumnos");
+  const alumnos = await response.json();
+  console.log(alumnos);
+}
+
   return (
     <>
       <div>
-        <h1>FUNCIONA</h1>
+        <button onClick={conectarBackend}>mostrar alumnos</button>
       </div>
     </>
   )
