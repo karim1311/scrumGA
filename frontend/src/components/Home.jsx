cc0dcd032b110e9f190d83ff533dab36ee546feb
karim1311 <valenzuelakarim03@gmail.com>
2023-10-27 17:02:11 -0700

    form

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -51,8 +51,17 @@ function Home() {
       fecha nacimiento:
       <input type="text" name='fecha_nacimiento' className='input-alumno'/>
       Grupo id:
-      <input type="text" name='grupo_id' className='input-alumno'/>
+      <select name="grupo_id" id="" className='input-alumno'>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+      </select>
+      {/* <input type="text" name='grupo_id' className='input-alumno'/> */}
       <button onClick={enviarDatos}>Guardar</button>
+
+
+      
     </>
       
 
