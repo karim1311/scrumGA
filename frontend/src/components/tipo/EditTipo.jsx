6a25c4aef2b32f3ed1bfbc787a10f34c53f737bc
pedroAntonio <pedro.salinas25@aol.com>
2023-10-31 10:26:22 -0500

    se comitea para cambiar de rama

--- frontend/src/components/tipo/EditTipo.jsx
+++ frontend/src/components/tipo/EditTipo.jsx
@@ -34,21 +34,40 @@ const EditTipo = () => {
 
     }, [])
     return (
-        <div>
-            <h2>Editar Tipo</h2>
-            <form onSubmit={update}>
+        <div className="flex flex-col justify-center items-center h-screen">
+            <div className="mt-5 text-amber-50">
+        <p className="text-xl mb-4">Edita Tipo Evaluación </p>
+        <p className="max-w-screen-sm h-14 text-sm p-2 rounded">
+          Bienvenido
+          <br />
+          Selecciona la accion que quieras realizar en las pestañas del menu de
+          la izquierda
+        </p>
+      </div>
+        <div className='crud'>
+        <h2 className="text-center font-bold text-2xl font-roboto mb-4">
+        Editar Tipo</h2>
+            <form onSubmit={update} className="flex-col flex items-center form_edit">
                 <div className='mb-3'>
-                    <label className='form-label'>Descripción</label>
+                    <label className='form-label'>
+                    <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+                Descripción
+              </span>
+                        </label>
                     <input
                         value={descripcion}
                         onChange={(e) => setDescripcion(e.target.value)}
                         type='text'
-                        className='form-control'
-                    />
+                        className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+            />
                 </div>
 
                 <div className='mb-3'>
-                    <label className='form-label'>Porcentaje</label>
+                    <label className='form-label'>
+                    <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+               Porcentaje
+              </span>
+                        </label>
                     <input
                         value={porcentaje}
                         onChange={(e) => setPorcentaje(e.target.value)}
@@ -68,6 +87,7 @@ const EditTipo = () => {
                 </div>
                 <button type='submit' className='btn btn-success'>Update</button>
             </form>
+        </div>
         </div>
     )
 }
