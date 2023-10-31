40b63fba07c70194ae3da46b196d2c529384bb33
karim1311 <valenzuelakarim03@gmail.com>
2023-10-31 12:39:58 -0700

    EditCalificacion.jsx

--- frontend/src/components/calificacion/EditCalificacion.jsx
+++ frontend/src/components/calificacion/EditCalificacion.jsx
@@ -5,94 +5,107 @@ import { useNavigate, useParams } from "react-router-dom";
 const endpoint = "http://localhost:8000/api/calificacion/";
 
 const EditCalificacion = () => {
-  const [descripcion, setDescripcion] = useState("");
-  const [porcentaje, setPorcentaje] = useState("");
-  const [rango, setRango] = useState("");
+  const [alumnoId, setAlumnoId] = useState("");
+  const [evaluacionId, setEvaluacionId] = useState("");
+  const [nota, setNota] = useState("");
+  const [mensaje, setMensaje] = useState("");
   const navigate = useNavigate();
   const { id } = useParams();
 
   const update = async (e) => {
     e.preventDefault();
     await axios.put(`${endpoint}${id}`, {
-      descripcion: descripcion,
-      porcentaje: porcentaje,
-      rango: rango,
+      alumno_id: alumnoId,
+      evaluacion_id : evaluacionId,
+      nota: nota,
+      mensaje: mensaje,
     });
-    navigate("/");
+    navigate("/calificaciones");
   };
 
   useEffect(() => {
-    const getTiposById = async () => {
+    const getCalificacionesById = async () => {
       const response = await axios.get(`${endpoint}${id}`);
-      setDescripcion(response.data.descripcion);
-      setPorcentaje(response.data.porcentaje);
-      setRango(response.data.rango);
+      setAlumnoId(response.data.alumno_id);
+      setEvaluacionId(response.data.evaluacion_id);
+      setNota(response.data.nota);
+      setMensaje(response.data.mensaje);
     };
-    getTiposById();
+    getCalificacionesById();
   }, []);
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <div className="mt-5 text-amber-50">
-        <p className="text-xl mb-4">Edita Tipo Evaluación </p>
-        <p className="max-w-screen-sm h-14 text-sm p-2 rounded">
-          Bienvenido
-          <br />
-          Selecciona la accion que quieras realizar en las pestañas del menu de
-          la izquierda
-        </p>
-      </div>
-      <div className="crud">
+    <div className="flex flex-col justify-center items-center h-screen w-screen">
+      <div className="mt-5 text-amber-50"></div>
+      <div className="flex flex-col items-center w-[70%]">
+      <div className="edit">
       <h2 className="text-center font-bold text-2xl font-roboto mb-4">
-          Editar Tipo</h2>
+          Editar Calificacion</h2>
         <form onSubmit={update} className="flex-col flex items-center form_edit">
-          <div className="mb-3">
+          <div className="mb-3 block">
             <label className="form-label">
             <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-                Descripción
+                Alumno ID
               </span>
                 </label>
             <input
-              value={descripcion}
-              onChange={(e) => setDescripcion(e.target.value)}
+              value={alumnoId}
+              onChange={(e) => setAlumnoId(e.target.value)}
               type="text"
-              className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+              className="mt-1 px-3 py-2 bg-[#323744] border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
             />
           </div>
 
           <div className="mb-3">
             <label className="form-label">
             <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-                Porcentaje
+                Evaluacion
               </span>
                 </label>
             <input
-              value={porcentaje}
-              onChange={(e) => setPorcentaje(e.target.value)}
+              value={evaluacionId}
+              onChange={(e) => setEvaluacionId(e.target.value)}
               type="text"
-              className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+              className="mt-1 px-3 py-2 bg-[#323744]  border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
             />
           </div>
 
           <div className="mb-3">
             <label className="form-label">
             <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-                Rango
+                Nota
               </span>
                 </label>
             <input
-              value={rango}
-              onChange={(e) => setRango(e.target.value)}
+              value={nota}
+              onChange={(e) => setNota(e.target.value)}
               type="text"
-              className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+              className="mt-1 px-3 py-2 bg-[#323744]  border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
             />
           </div>
-          <button type="submit" className="btn btn-success button">
+          <div className="mb-3">
+            <label className="form-label">
+            <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+                Mensaje
+              </span>
+                </label>
+            <input
+              value={mensaje}
+              onChange={(e) => setMensaje(e.target.value)}
+              type="text"
+              className="mt-1 px-3 py-2 bg-[#323744]  border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+            />
+          </div>
+          <button type="submit" className="button">
             Update
           </button>
         </form>
+      </div>
       </div>
     </div>
   );
 };
 
 export default EditCalificacion;
+
+
+
